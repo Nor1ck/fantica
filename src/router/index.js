9cef3f341002e6fb365fb6d0d8cccec71e10f430
fanticadapp <fanticadapp@gmail.com>
2020-09-15 18:21:17 +0400

    router

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,25 @@ Vue.use(VueRouter)
     component: Home
   },
   {
+    path: '/profile',
+    name: 'Profile',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Profile.vue')
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/newpost',
+    name: 'NewPost',
+    component: () => import(/* webpackChunkName: "about" */ '../views/NewPost.vue')
+  },
+  {
+    path: '/:username',
+    name: 'Feed',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Feed.vue')
+  },
 ]
 
 const router = new VueRouter({
