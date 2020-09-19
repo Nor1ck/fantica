443c0c839e652a6e388b29ec86bb27190a261984
fanticadapp <fanticadapp@gmail.com>
2020-09-19 20:05:32 +0400

    simple homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>Fantica Home</h1>
+    <!-- <v-btn rounded color="purple accent-1" @click="buyFNT()">Buy FNT</v-btn>
+    <v-btn rounded color="yellow darken-1" @click="buyFNT()">Buy DAI</v-btn> -->
   </div>
 </template>
 
@@ -14,6 +16,12 @@ export default {
     return {};
   },
   methods: {
+    buyFNT() {
+
+    },
+    buyDAI() {
+
+    },
   },
 };
 </script>
