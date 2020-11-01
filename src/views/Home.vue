262548d5500b50312955227bdb864530c6a76927
fanticadapp <fanticadapp@gmail.com>
2020-11-02 00:18:18 +0400

    FNT URLs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,30 @@
     <h1>Fantica Home</h1>
 
     <p>
-      Fantica DApp: <a :href="fanticaDAppURL" target="_blank">{{ fanticaDAppAddress }}</a>
+      Fantica DApp:
+      <a :href="fanticaDAppURL" target="_blank">{{ fanticaDAppAddress }}</a>
+    </p>
+
+    <p>
+      <a
+        href="https://info.uniswap.org/token/0x3d23f2e86a9ab43561203b143c18ad9de0b5c745"
+        target="_blank"
+        >Uniswap Fantica (FNT) Info</a
+      >
+    </p>
+    <p>
+      <a
+        href="https://uniswap.info/pair/0xcf35b2923cf8cc152846194592b69a08180cdaea"
+        target="_blank"
+        >Uniswap FNT-ETH Pair</a
+      >
+    </p>
+    <p>
+      <a
+        href="https://etherscan.io/address/0x3d23f2e86a9ab43561203b143c18ad9de0b5c745#code"
+        target="_blank"
+        >Fantica ERC-20 Contract</a
+      >
     </p>
 
     <!-- <v-btn rounded color="purple accent-1" @click="buyFNT()">Buy FNT</v-btn>
